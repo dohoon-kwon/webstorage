fd40580993546fc464f3bad7c525676ec44a9954
KyoungSeopYang <5428136@naver.com>
2020-09-25 15:25:07 +0900

    업로드 페이지 UI 정리

--- upload.php
+++ upload.php
@@ -2,6 +2,7 @@
     session_start();
     $id=$_SESSION['id'];
 ?>
+
 <!doctype html>
 <html>
 <head>
@@ -9,7 +10,9 @@
   <title>파일 저장소</title>
   <link rel="stylesheet" href="css/default.css">
   <link rel="stylesheet" href="css/main.css">
-  <link rel="stylesheet" href="css/upload.css">
+  <link rel="stylesheet" href="css/upload.css?=ver1">
+  <script src="js/upload.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
     <nav>
@@ -18,10 +21,9 @@
             <li><a class="logout" href="login.php">로그아웃</a></li>
         </ul>
         <ul class="top_menu">
-            <li class="top_menu_item"><a href="main.php">홈페이지 정보</a></li>
-            <li class="top_menu_item"><a href="upload.php">파일 저장소</a></li>
-            <li class="top_menu_item"><a href="board.php">게시판 정보</a></li>
-            <li class="top_menu_item"><a href="tool.php">홈페이지 관리</a></li>
+          <li class="top_menu_item" onclick="location.href='upload.php'"><a>파일 저장소</a></li>
+            <li class="top_menu_item" onclick="location.href='board.php'"><a>게시판 정보</a></li>
+            <li class="top_menu_item" onclick="location.href='tool.php'"><a>홈페이지 관리</a></li>
         </ul>
     </nav>
     <div class="rightmenu">
@@ -32,58 +34,16 @@
             <li><h1>저장소 용량표시도 만들어야할까</h1></li>
         </ul>
     </div>
-    <form enctype="multipart/form-data" action="uploadprocess.php" method="POST" class="leftmenu">
-        <input type="hidden" name="MAX_FILE_SIZE" value="99999999999999999" />
-        <input type="hidden" name="id" value="<?=$_GET['id']?>"/>
-        <input name="userfile" type="file" />
-        <input type="submit" value="업로드" />
-    </form>
-
-    <div id="drop_file_zone" ondrop="upload_file(event)" ondragover="return false">
+    <div id="content">
+      <div id="drop_file_zone" ondrop="upload_file(event)" ondragover="return false">
           <div id="drag_upload_file">
             <p>Drop file here</p>
             <p>or</p>            
             <p><input type="button" value="Select File" onclick="file_explorer();"></p>
             <input type="file" id="selectfile" name="userfile">
           </div>
-        </div>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-    <script type="text/javascript">
-      var fileobj;
-      function upload_file(e) {
-        e.preventDefault();
-        fileobj = e.dataTransfer.files[0];
-        ajax_file_upload(fileobj);
-      }
-
-      function file_explorer() {
-        document.getElementById('selectfile').click();
-        document.getElementById('selectfile').onchange = function() {
-            fileobj = document.getElementById('selectfile').files[0];
-          ajax_file_upload(fileobj);
-        };
-      }
-
-      function ajax_file_upload(file_obj) {
-        if(file_obj != undefined) {
-            var form_data = new FormData();                  
-            form_data.append('file', file_obj);
-          $.ajax({
-            type: 'POST',
-            url: 'uploadprocess.php',
-            contentType: false,
-            processData: false,
-            data: form_data,
-            success:function(response) {
-              alert(response);
-              $('#selectfile').val('');
-            }
-          });
-        }
-      }
-    </script>
+      </div>
+  </div>
 
 </body>
 </html>
